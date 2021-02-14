f4d66db1dbf1405ce845217bdcb5fe82592277c6
Jun Zhang <albrecht.jun.zhang@gmail.com>
2021-02-14 13:45:23 -0400

    init commit

---
+++ CustomRenderer/ScanResultPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CustomRenderer
+{
+    public partial class ScanResultPage : ContentPage
+    {
+        public string ScanResult { get; set; }
+
+        public ScanResultPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            resultLabel.Text = ScanResult;
+        }
+    }
+}
